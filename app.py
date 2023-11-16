abbf3ba79e6aedf7eb77ee16a09cfddccc082803
Kishan <payadikishan@gmail.com>
2023-11-17 04:03:47 +0530

    routing functions done

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+####Routing Functions####\
+
+
 @app.route('/')
 def home():
+    
     return render_template('home.html', mess = "There is no model Please add one")
+
+
+@app.route("/start", methods=['GET'])
+def start():
+    return "This is start page"
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    return "This is add page"
 
 if __name__ == "__main__":
     app.run(debug=True)
