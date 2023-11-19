5833e650c69a175753d9c7b286e8bea8efc52a2b
Kishan <payadikishan@gmail.com>
2023-11-19 16:14:22 +0530

    'add' route added

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import os
+import cv2
 app = Flask(__name__)
 
+nimgs = 10
+face_detector = cv2.CascadeClassifier('static\haarcascade_frontalface_default.xml')
+
+def extract_faces(img):
+    try:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_points = face_detector.detectMultiScale(gray, 1.2, 5, minSize=(20, 20))
+        return face_points
+    except:
+        return []
 
 ####Routing Functions####\
 
@@ -18,6 +29,37 @@ def start():
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
+    newusername = request.form['newusername']
+    newuserid = request.form['newuserid']
+    userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
+    if not os.path.isdir(userimagefolder):
+        os.makedirs(userimagefolder)
+    i, j = 0, 0
+    cap = cv2.VideoCapture(0)
+    while 1:
+        _, frame = cap.read()
+        faces = extract_faces(frame)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
+            cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+            if j % 5 == 0:
+                name = newusername+'_'+str(i)+'.jpg'
+                cv2.imwrite(userimagefolder+'/'+name, frame)
+                i += 1
+            j += 1
+            if j == nimgs*5:
+                break
+            cv2.imshow('Adding new User', frame)
+            if cv2.waitKey(1) == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+        print('Training Model')
+        # train_model()
+        # names, rolls, times, l = extract_attendance()
+        return render_template('home.html')
+
     return "This is add page"
 
 if __name__ == "__main__":
